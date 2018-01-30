d7b4e5c65fecca6927fbbfb8df023ba9343cce0b
Antoine <s.github@2ec0b4.fr>
2018-01-30 13:48:08 +0100

    Date and time preferences

--- init/macos.sh
+++ init/macos.sh
@@ -194,11 +194,37 @@ defaults write NSGlobalDomain AppleMetricUnits -bool true
 # Hide language menu in the top right corner of the boot screen
 sudo defaults write /Library/Preferences/com.apple.loginwindow showInputMenu -bool false
 
-# Set the timezone; see `sudo systemsetup -listtimezones` for other values
-sudo systemsetup -settimezone "Europe/Paris" > /dev/null
-
 # Stop iTunes from responding to the keyboard media keys
 launchctl unload -w /System/Library/LaunchAgents/com.apple.rcd.plist 2> /dev/null
+
+###############################################################################
+# Date & Time                                                                 #
+###############################################################################
+
+# Set the timezone; see `systemsetup -listtimezones` for other values
+sudo systemsetup -settimezone "Europe/Paris" > /dev/null
+
+# Set date and time automatically
+sudo systemsetup -setusingnetworktime on > /dev/null
+
+# Set time server
+sudo systemsetup -setnetworktimeserver "time.apple.com" > /dev/null
+
+# Set time zome automatically using current location
+sudo defaults write /Library/Preferences/com.apple.timezone.auto.plist Active -bool false
+
+# Menu bar clock format
+# "h:mm" Default
+# "HH"   Use a 24-hour clock
+# "a"    Show AM/PM
+# "ss"   Display the time with seconds
+defaults write com.apple.menuextra.clock DateFormat -string "HH:mm:ss"
+
+# Flash the time separators
+defaults write com.apple.menuextra.clock FlashDateSeparators -bool false
+
+# Analog menu bar clock
+defaults write com.apple.menuextra.clock IsAnalog -bool false
 
 ###############################################################################
 # Screen                                                                      #
