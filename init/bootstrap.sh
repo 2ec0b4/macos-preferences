1342d33c90daacb32a768e1ea8cd008ff93500c5
Antoine <s.github@2ec0b4.fr>
2018-02-06 17:23:34 +0100

    Exclude a directory to rsync

--- init/bootstrap.sh
+++ init/bootstrap.sh
@@ -7,6 +7,7 @@ git pull origin master;
 function doIt() {
 	rsync --exclude ".git/" \
 		--exclude ".DS_Store" \
+		--exclude ".idea" \
 		--exclude "init/" \
 		--exclude "README.md" \
 		--exclude "LICENSE-MIT.txt" \
